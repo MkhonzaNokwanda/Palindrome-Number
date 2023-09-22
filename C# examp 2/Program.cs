dfb5b5db355f8040cbddea6c662ff9a816086b7e
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-22 15:00:52 +0200

    Add project files.

---
+++ C# examp 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__examp_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration of variables
+            int x;
+            int r;
+            int sum = 0;
+            int temp;
+
+            Console.WriteLine("Enter a number");
+            x=int.Parse(Console.ReadLine());
+            temp = x;
+
+            while (x > 0)
+            {
+                r = x % 10;
+                sum = (sum * 10) + r;
+                x = x / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("Number is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Number is not palindrome");
+            }
+
+            //freeze console
+            Console.ReadLine();
+
+        }
+    }
+}
